a477da17696e581c7643f7eba5a2a0c3a110964a
CapdamDev <programacion@capdam.gob.mx>
2023-10-26 13:48:15 -0600

    Cambio de rutas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* SIGN IN page */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+/* SIGN UP page */
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+/* DASHBOARD page */
+
+router.get('/dashboard', function(req, res, next) {
+  res.render('dashboard');
 });
 
 module.exports = router;
