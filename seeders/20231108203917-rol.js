574983e050909f2666e8111b44d3fce37f9dbf8d
CapdamDev <programacion@capdam.gob.mx>
2023-11-10 12:36:46 -0600

    Actualización de seeders base para la inicialización de cero del proyecto

--- seeders/20231108203917-rol.js
+++ seeders/20231108203917-rol.js
@@ -2,16 +2,16 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-      await queryInterface.bulkInsert('roles', [{
-          role_name: 'admin',
-          role_description: 'System Admin',
-          createdAt: new Date(),
-          updatedAt: new Date()
-      }], {});
-  },
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkInsert('roles', [{
+            role_name: 'admin',
+            role_description: 'System Admin',
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }], {});
+    },
 
-  down: async (queryInterface, Sequelize) => {
-      await queryInterface.bulkDelete('roles', null, {});
-  }
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.bulkDelete('roles', null, {});
+    }
 };
