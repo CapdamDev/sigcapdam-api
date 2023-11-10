32f1e1db3c77f4cb906390ec3515e0c89da61ab9
CapdamDev <programacion@capdam.gob.mx>
2023-11-10 12:32:40 -0600

    Se agrega una pre-estructura para agregar nuevas layers

--- routes/layers.js
+++ routes/layers.js
@@ -1,11 +1,37 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
-const passport = require("passport");
 const router = express.Router();
-require("../config/passport")(passport);
-const User = require("../models").User;
-const Role = require("../models").Role;
 const Layer = require("../models").Layer;
+const passport = require("passport");
+require("../config/passport")(passport);
+const Helper = require("../utils/helper");
+const helper = new Helper();
+
+// Create a new Layer
+router.post('/', passport.authenticate('jwt', {
+	session: false
+}), function (req, res) {
+	helper.checkPermission(req.user.role_id, 'layer_add').then((rolePerm) => {
+		if (!req.body.name || !req.body.category || !req.body.content) {
+			res.status(400).send({
+				msg: 'Please pass name, category and content.'
+			})
+		} else {
+			Layer
+				.create({
+					name: req.body.name,
+					category: req.body.category,
+					content: req.body.content
+				})
+				.then((layer) => res.status(201).send(layer))
+				.catch((error) => {
+					console.log(error);
+					res.status(400).send(error);
+				});
+		}
+	}).catch((error) => {
+		res.status(403).send(error);
+	});
+});
 
 // Consulta todas las layers
 router.get("/all", function (req, res) {
@@ -19,10 +45,10 @@ router.get("/all", function (req, res) {
 // Consulta una layer por nombre
 router.get("/:name", function (req, res) {
 	Layer.findOne({
-		where: {
-			name: req.params.name,
-		},
-	})
+			where: {
+				name: req.params.name,
+			},
+		})
 		.then((layer) => res.status(201).send(layer))
 		.catch((error) => {
 			res.status(400).send(error);
@@ -32,25 +58,25 @@ router.get("/:name", function (req, res) {
 // Actualiza una layer cuando se modifica su contenido
 router.put("/:name", function (req, res) {
 	Layer.findOne({
-		where: {
-			name: req.params.name,
-		}
-	})
-	.then(layer => {
-		if(!layer) {
-			return res.status(404).send({
-				message: 'Layer Not Found',
-			});
-		}
-
-		Layer.update({
-			content: req.body.content || layer.content,
-			updatedAt: new Date()
+			where: {
+				name: req.params.name,
+			}
 		})
-		.then(() => res.status(200).send(layer))
+		.then(layer => {
+			if (!layer) {
+				return res.status(404).send({
+					message: 'Layer Not Found',
+				});
+			}
+
+			Layer.update({
+					content: req.body.content || layer.content,
+					updatedAt: new Date()
+				})
+				.then(() => res.status(200).send(layer))
+				.catch(error => res.status(400).send(error));
+		})
 		.catch(error => res.status(400).send(error));
-	})
-	.catch(error => res.status(400).send(error));
 });
 
 module.exports = router;
