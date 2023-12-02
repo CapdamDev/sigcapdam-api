a6fc7b4b2b1285ece0c6f99c605854c9e2ef7aa8
CapdamDev <programacion@capdam.gob.mx>
2023-12-02 12:00:53 -0600

    Se agrega el guardado de cookies seguras para poder usarlas en las vistas desde el lado del servidor

--- routes/auth.js
+++ routes/auth.js
@@ -1,111 +1,121 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const passport = require('passport');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const passport = require("passport");
 const router = express.Router();
-require('../config/passport')(passport);
-const User = require('../models').User;
-const Role = require('../models').Role;
-const Permission = require('../models').Permission;
-const RolePermission = require('../models').RolePermission;
+require("../config/passport")(passport);
+// const User = require('../models').User;
+// const Role = require('../models').Role;
+// const Permission = require('../models').Permission;
+// const RolePermission = require('../models').RolePermission;
+const { User, Role, Permission, RolePermission } = require("../models");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
 // Archivo de login y registro de usuarios
 
-router.post('/register', function (req, res) {
-    if (!req.body.email || !req.body.password || !req.body.nombre) {
-        res.status(400).send({
-            msg: 'Ingresa un nombre o contraseña válidos.'
-        })
-    } else {
-        Role.findOne({
-            where: {
-                role_name: 'admin'
-            }
-        }).then((role) => {
-            console.log(role.id);
-            User.create({
-                nombre: req.body.nombre,
-                ape_pat: req.body.ape_pat,
-                ape_mat: req.body.ape_mat,
-                email: req.body.email,
-                password: req.body.password,
-                isActive: req.body.isActive,
-                role_id: role.id
-            })
-            .then((user) => res.status(201).send(user))
-            .catch((error) => {
-                res.status(400).send(error);
-            });
-        }).catch((error) => {
-            res.status(400).send(error);
-        });
-    }
+router.post("/register", function (req, res) {
+	if (!req.body.email || !req.body.password || !req.body.nombre) {
+		res.status(400).send({
+			msg: "Ingresa un nombre o contraseña válidos.",
+		});
+	} else {
+		Role.findOne({
+			where: {
+				role_name: "admin",
+			},
+		})
+			.then((role) => {
+				console.log(role.id);
+				User.create({
+					nombre: req.body.nombre,
+					ape_pat: req.body.ape_pat,
+					ape_mat: req.body.ape_mat,
+					email: req.body.email,
+					password: req.body.password,
+					isActive: req.body.isActive,
+					role_id: role.id,
+				})
+					.then((user) => res.status(201).send(user))
+					.catch((error) => {
+						res.status(400).send(error);
+					});
+			})
+			.catch((error) => {
+				res.status(400).send(error);
+			});
+	}
 });
 
-router.post('/login', function (req, res) {
-    User.findOne({
-            where: {
-                email: req.body.email
-            }
-        })
-        .then((user) => {
-            if (!user) {
-                return res.status(401).send({
-                    message: 'Authentication failed.',
-                });
-            }
-            user.comparePassword(req.body.password, (err, isMatch) => {
-                if (isMatch && !err) {
-                    var token = jwt.sign(JSON.parse(JSON.stringify(user)), 'nodeauthsecret', {
-                        expiresIn: 86400 * 30
-                    });
-                    jwt.verify(token, 'nodeauthsecret', function (err, data) {
-                        console.log(err, data);
-                    })
-                    RolePermission.findAll({ where: { role_id: user.role_id } })
-                        .then(RolePermission => {
-                            var permIds = RolePermission.map(rp => rp.perm_id);
-                            Permission.findAll({ where: { id: permIds } })
-                                .then(permissions => {
-                                    var permNames = permissions.map(p => p.perm_name);
+router.post("/login", function (req, res) {
+	User.findOne({
+		where: {
+			email: req.body.email,
+		},
+	})
+		.then((user) => {
+			if (!user) {
+				return res.status(401).send({
+					message: "Authentication failed.",
+				});
+			}
 
-                                    // Fetch role details based on role_id
-                                    Role.findOne({ where: { id: user.role_id } })
-                                        .then(role => {
-                                            res.send({
-                                                success: true,
-                                                token: 'JWT ' + token,
-                                                role_id: user.role_id,
-                                                role_name: role.role_name, // Add role_name to the response
-                                                user_email: user.email,
-                                                permissions: permNames,
-                                            });
-                                        })
-                                        .catch(error => {
-                                            res.status(400).send(error);
-                                        });
-                                });
-                        });
-                } else {
-                    res.status(401).send({
-                        success: false,
-                        msg: 'Credenciales incorrectas.'
-                    });
-                }
-            })
-        })
-        .catch((error) => res.status(400).send(error));
+			user.comparePassword(req.body.password, (err, isMatch) => {
+				if (isMatch && !err) {
+					var token = jwt.sign(
+						JSON.parse(JSON.stringify(user)),
+						"nodeauthsecret",
+						{
+							expiresIn: 86400 * 30,
+						}
+					);
+
+					// Save cookies immediately
+					res.cookie("token", "JWT " + token, { httpOnly: true, secure: true });
+
+					RolePermission.findAll({ where: { role_id: user.role_id } }).then(
+						(RolePermission) => {
+							var permIds = RolePermission.map((rp) => rp.perm_id);
+							Permission.findAll({ where: { id: permIds } }).then(
+								(permissions) => {
+									var permNames = permissions.map((p) => p.perm_name);
+
+									// Fetch role details based on role_id
+									Role.findOne({ where: { id: user.role_id } })
+										.then((role) => {
+											res.cookie("role_id", user.role_id);
+											res.cookie("role_name", role.role_name); // Assuming user.role_name is accessible
+											res.cookie("user_email", user.email);
+											res.cookie("user_name", user.nombre);
+											res.send({ success: true });
+										})
+										.catch((error) => {
+											res.status(400).send(error);
+										});
+								}
+							);
+						}
+					);
+				} else {
+					res.status(401).send({
+						success: false,
+						msg: "Credenciales incorrectas.",
+					});
+				}
+			});
+		})
+		.catch((error) => res.status(400).send(error));
 });
 
 // Logout route
-router.post('/logout', (req, res) => {
-    res.send({
-        success: true,
-        token: null,
-        role_id: null,
-        role_name: null,
-        user_email: null,
-        permissions: null,
-    })
+router.post("/logout", (req, res) => {
+	// Clear cookies
+	res.clearCookie("token");
+	res.clearCookie("role_id");
+	res.clearCookie("role_name");
+	res.clearCookie("user_email");
+
+	// Send JSON to confirm delete
+	res.json({ success: true });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,88 +3,74 @@ var router = express.Router();
 var passport = require('passport');
 var Layer = require('../models').Layer;
 var Category = require('../models').Category;
+var cookieParser = require('cookie-parser');
 
-// Router general del proyecto, 
+// Utiliza el middleware cookie-parser para analizar las cookies
+router.use(cookieParser());
+
+// Middleware para procesar las cookies
+router.use(function(req, res, next) {
+  // Accede a las cookies y almacénalas en el objeto req para que estén disponibles en todas las rutas
+  req.parsedCookies = req.cookies;
+  next();
+});
+
+// Router general del proyecto
 
 /* GET de la pagina de inicio */
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
   res.render('login');
 });
-
-// router.post('/login', async function(req, res, next) {
-//   const { email, password } = req.body;
-//   console.log(email, password);
-
-//   if (email && password) {
-//     try {
-//       const response = await fetch('http://localhost:3000/login', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify({ email, password }),
-//       });
-
-//       const data = await response.json();
-
-//       if (data.error) {
-//         res.send(data.error);
-//         // res.render('login', { error: data.error });
-//       } else {
-//         // Save the JWT token in the session to make it available to the angular app
-//         // success: true,
-//         // token: 'JWT ' + token,
-//         // role_id: user.role_id,
-//         // role_name: role.role_name, // Add role_name to the response
-//         // user_email: user.email,
-//         // permissions: permNames,
-//         res.redirect(`/home?token=${data.token}&role_id=${data.role_id}&role_name=${data.role_name}&user_email=${data.user_email}&permissions=${data.permissions}`);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//     }
-//   }
-// });
 
 /* GET de la pagina del login */
-router.get('/login', function(req, res, next) {
+router.get('/login', async function(req, res, next) {
   res.render('login');
 });
 
-/* GET de las layers */
-router.get('/home',async function(req, res, next) {
-  const { token, role_id, role_name, user_email, permissions } = req.query;
-  console.log(token, role_id, role_name, user_email, permissions);
-  res.render('home', { token, role_id, role_name, user_email, permissions });
+/* GET de las layers en el home */
+router.get('/home', async function(req, res, next) {
+  const cookies = req.parsedCookies;
+  res.render('home', { cookies } );
 });
 
 /* GET layers_dashboard */
 router.get('/layers_dashboard', async function (req, res, next) {
-  try {
-    // Fetch layers data from the API endpoint
-    const response = await fetch('http://localhost:3000/api/v1/layers/all', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    const layers = await response.json();
-    // Render the 'layers_dashboard' view and pass the layers data
-    res.render('layers_dashboard', { layers });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
+  const cookies = req.parsedCookies;
+
+  if(!token){
+    res.redirect('/login');
+  } else{
+      try {
+        // Fetch layers data from the API endpoint
+        const response = await fetch('http://localhost:3000/api/v1/layers/all', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': cookies.token,
+          },
+        });
+        const layers = await response.json();
+
+        if(!layers){
+          res.send('Error: Could not fetch layers data');
+        }
+        else{
+          // res.send(layers);
+          res.render('layers_dashboard', { layers });
+        }
+      } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+      }
   }
 });
 
 /* GET users_dashboard */
-router.get('/users_dashboard', function(req, res, next) {
+router.get('/users_dashboard', async function(req, res, next) {
   res.render('users_dashboard');
 });
 
 /* GET settings */
-router.get('/settings', function(req, res, next) {
+router.get('/settings', async function(req, res, next) {
   res.render('settings');
 });
 
@@ -103,6 +89,5 @@ router.get('/settings', function(req, res, next) {
 // router.get('/layers2', function(req, res, next) {
 //   res.render('layers2');
 // });
-
 
 module.exports = router;
