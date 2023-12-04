c665657dfc61e8d6bbede44dd306de450da0170d
CapdamDev <programacion@capdam.gob.mx>
2023-12-04 09:45:56 -0600

    Se agrega el campo profilePic a la tabla de los usuarios

--- models/user.js
+++ models/user.js
@@ -19,6 +19,10 @@ module.exports = (sequelize, DataTypes) => {
   };
   User.init({
     role_id: DataTypes.INTEGER,
+    profilePic: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     nombre: {
       type: DataTypes.STRING,
       allowNull: false,
--- seeders/20231115185907-users.js
+++ seeders/20231115185907-users.js
@@ -17,6 +17,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('rolepermissions', null, {});
+    await queryInterface.bulkDelete('users', null, {});
   }
 };
