acb9bf7f4f7b81ca042a0170ed48b86254aa9451
CapdamDev <programacion@capdam.gob.mx>
2023-12-06 15:59:48 -0600

    Preparacion del CRUD para las capas

---
+++ migrations/20231206213519-addLayerIcon.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.addColumn(
+        'layers',
+        'icono',
+        {
+          type: Sequelize.STRING,
+          allowNull: true
+        }
+      ),
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    return Promise.all([
+      queryInterface.removeColumn('layers', 'icono'),
+    ])
+  }
+};
