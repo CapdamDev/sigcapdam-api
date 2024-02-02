136e393f45b9f59658ef9e7ce35c8aaa089767b6
CapdamDev <programacion@capdam.gob.mx>
2024-02-02 13:03:51 -0600

    Agregada columna de isActive para prevenir borrados en la base de datos

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
-// Configuración del ORM Sequelize para el uso en el servidor
-
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize({
-	dialect: process.env.DB_DIALECT,
+	dialect: process.env.DB_DIALECT, // Specify the database dialect (e.g., 'mysql', 'postgres', 'sqlite')
 	host: process.env.DB_HOST,
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
 });
+
+module.exports = sequelize;
