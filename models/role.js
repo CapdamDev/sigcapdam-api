1c29636cb658fe68afe7e080cd56810f2d7b8db8
CapdamDev <programacion@capdam.gob.mx>
2024-02-07 13:31:20 -0600

    Reconfiguración de la conexión de Sequelize y corrección de consulta de datos de Dirección y Departamentos

--- models/role.js
+++ models/role.js
@@ -30,6 +30,11 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
     },
     {
       sequelize,
