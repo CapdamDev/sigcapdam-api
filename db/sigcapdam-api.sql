afde6914e3f2906c4b28a2c672cfc03d69d97af6
CapdamDev <programacion@capdam.gob.mx>
2024-03-12 13:03:50 -0600

    Update de la base de datos

--- db/sigcapdam-api.sql
+++ db/sigcapdam-api.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 27-02-2024 a las 17:37:50
+-- Tiempo de generación: 12-03-2024 a las 20:03:11
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -154,7 +154,7 @@ CREATE TABLE IF NOT EXISTS `Layers` (
   `icono` varchar(255) DEFAULT NULL,
   `isActive` tinyint(1) NOT NULL DEFAULT 1,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=197 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=216 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `Layers`
